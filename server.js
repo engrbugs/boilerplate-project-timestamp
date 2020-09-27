e58a98b12563b03464d2d7e455a8b6dc034e2fbe
engrbugs <engrbugs@gmail.com>
2020-09-26 22:27:45 -0700

    done

--- server.js
+++ server.js
@@ -19,37 +19,34 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-
+// your first API endpoint...
 app.get("/api/timestamp/", function (req, res) {
   var date = new Date();
   res.json({
-      unix: date.getTime(),
-      utc: date.toUTCString()
-    });
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
 });
 
-// your first API endpoint... 
 app.get("/api/timestamp/:date_str", function (req, res) {
-  console.log('DATA:', req.params.date_str);
+  console.log("DATA:", req.params.date_str);
   var date = new Date(req.params.date_str);
-  console.log('date:', date);
-  if (date.toString() === 'Invalid Date') {
+  console.log("date:", date);
+  if (date.toString() === "Invalid Date") {
     date = new Date(parseInt(req.params.date_str));
-  };
-  console.log('date:', date); 
-  if (date.toString() === 'Invalid Date') {
+  }
+  console.log("date:", date);
+  if (date.toString() === "Invalid Date") {
     return res.json({
-      error: "Invalid Date"
+      error: "Invalid Date",
     });
   } else {
     return res.json({
       unix: date.getTime(),
-      utc: date.toUTCString()
+      utc: date.toUTCString(),
     });
-  };
+  }
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(port, function () {
