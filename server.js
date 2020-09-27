32fba731ac913cdd77887a1558c6b7c886a1dca7
engrbugs <engrbugs@gmail.com>
2020-09-26 20:17:38 -0700

    done

--- server.js
+++ server.js
@@ -19,10 +19,39 @@ app.get("/*", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/api/timestamp/", (req, res) => {
+
+app.get("/api/timestamp/", function (req, res) {
   var date = new Date();
-  res.json({greeting: 'hello API'});
+  res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
 });
+
+
+// your first API endpoint... 
+app.get("/api/timestamp/:date_str", function (req, res) {
+  const { date_str } = req.params;
+  console.log('DATA:', date_str);
+  var date = new Date(date_str);
+  console.log('date:', date);
+  if (date.toString() === 'Invalid Date') {
+    date = new Date(parseInt(date_str));
+  };
+  console.log('date:', date); 
+  if (date.toString() === 'Invalid Date') {
+    return res.json({
+      error: "Invalid Date"
+    });
+  } else {
+    return res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  };
+});
+
+
 
 // listen for requests :)
 var listener = app.listen(port, function () {
