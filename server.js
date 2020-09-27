fd44d26dbe2fa31c68707fbbb74344f58cd49305
engrbugs <engrbugs@gmail.com>
2020-09-26 18:23:31 -0700

    done

--- server.js
+++ server.js
@@ -19,17 +19,26 @@ app.get("/", function (req, res) {
 });
 
 
+app.get("/api/timestamp/", function (req, res) {
+  let date = new Date();
+  res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+});
+
+
 // your first API endpoint... 
 app.get("/api/timestamp/:date_str", function (req, res) {
   const { date_str } = req.params;
-
+  console.log('DATA:', date_str);
   let date = new Date(date_str);
-
-  if (date.toString() === 'InvalidDate') {
+  console.log('date:', date);
+  if (date.toString() === 'Invalid Date') {
     date = new Date(parseInt(date_str));
   };
-
-  if (date.toString() === 'InvalidDate') {
+  console.log('date:', date); 
+  if (date.toString() === 'Invalid Date') {
     return res.json({
       error: "Invalid Date"
     });
