720dca12b2223b5a2ec6ebe58accab8da5dd2e4e
engrbugs <engrbugs@gmail.com>
2020-09-26 18:52:37 -0700

    done

--- server.js
+++ server.js
@@ -18,40 +18,6 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-app.get("/api/timestamp/", function (req, res) {
-  let date = new Date();
-  res.json({
-      unix: date.getTime(),
-      utc: date.toUTCString()
-    });
-});
-
-
-// your first API endpoint... 
-app.get("/api/timestamp/:date_str", function (req, res) {
-  const { date_str } = req.params;
-  console.log('DATA:', date_str);
-  let date = new Date(date_str);
-  console.log('date:', date);
-  if (date.toString() === 'Invalid Date') {
-    date = new Date(parseInt(date_str));
-  };
-  console.log('date:', date); 
-  if (date.toString() === 'Invalid Date') {
-    return res.json({
-      error: "Invalid Date"
-    });
-  } else {
-    return res.json({
-      unix: date.getTime(),
-      utc: date.toUTCString()
-    });
-  };
-});
-
-
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
